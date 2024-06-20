cd425f21bf5f363cec666793c0006e62651ee1c3
Mayank Bhatt <161678215+mayankgith@users.noreply.github.com>
2024-06-20 20:46:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+function run()
+{
+	var code = document.getElementById("code").value;
+	var result_box = document.getElementById("result");
+	result_box.innerHTML = code;
+	var code_box = document.getElementById("code_box");
+	code_box.style.display = "none";
+	var result_box = document.getElementById("result_box");
+	result_box.style.display = "block";
+	result_box.setAttribute("class","animate__animated animate__slideInRight");
+	var btn = document.getElementById("run_btn");
+	btn.style.display = "none";
+	var back_btn = document.getElementById("icon");
+	back_btn.style.display = "block"; 
+}
+function back(){
+	var result_box = document.getElementById("result_box");
+	result_box.style.display = "none";
+	var code_box = document.getElementById("code_box");
+	code_box.style.display = "block";
+	code_box.setAttribute("class","animate__animated animate__slideInLeft");
+	var back_btn = document.getElementById("icon");
+	back_btn.style.display = "none";
+	var btn = document.getElementById("run_btn");
+	btn.style.display = "block"; 
+}
